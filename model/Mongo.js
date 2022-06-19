fe47fd08ba32ad0f5964988d67e4424e1c18ccab
SokhibjonDev <105203720+SokhibjonDev@users.noreply.github.com>
2022-06-19 19:13:48 +0500

    netflix adaptation
    feat:update post method

--- model/Mongo.js
+++ model/Mongo.js
@@ -5,10 +5,10 @@ const bookSchema = new Schema({
         type: String,
         required: true,
     },
-    price: {
-        type: Number,
-        required: true
-    },
+    // price: {
+    //     type: Number,
+    //     required: true
+    // },
     img: {
         type: String,
         required: true
